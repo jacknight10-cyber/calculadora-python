e0e4a3f83e1eb471c900752e51f0ffb6c573e4e3
jacknight10-cyber <jack.manjarres10@gmail.com>
2026-04-08 16:32:54 -0400

    ya termine por fin

--- calculadora-python/calculadora2.py
+++ calculadora-python/calculadora2.py
@@ -2,41 +2,39 @@ print ("Mi calculadora")
 # entrada
 
 #variable a
+
 a = float (input ("a:"))
 
 # operaciones
-#suma
-print ("+. suma")
-#resta
-print ("-. resta")
-#multiplicaion
-print ("*.multiplicaion")
-#division
-print ("/. division")
-operacion = input("signo de la operacion:")
+
+print ("+. más, -. menos, *. por, /. entre")
+operacion = input("signo:")
 
 #variables b
-b = float (input ("\n b:"))
+
+b = float (input ("b:"))
 
 #transformacion
+
 if operacion == "+":
     c = a + b
-    nombre_operacion = "suma"
+    nombre = "suma"
 elif operacion == "-":
     c = a - b
-    nombre_operacion = "resta"
+    nombre = "resta"
 elif operacion == "*":
-    nombre_operacion = "multiplicacion"
+    nombre = "multiplicacion"
 elif operacion == "/":
     if b != 0:  
         c = a / b
-        nombre_operacion = "division"
+        nombre = "division"
     else :
-        c = "error (operacion por 0) "
-        nombre_operacion = "division"
+        c = "no se puede dividir por 0"
+        nombre = "division"
+else:
+    c = "proximamente"
 
 #salida
-print ("\n---")
-print("resultado de la", nombre_operacion)
-print("es de:",c)
-print ("\n---fin de programa")
+
+print("el resultado es de", c)
+print ("fin de programa")
