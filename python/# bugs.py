5994a3e44175955e69289e1ca108e923b83c0809
jacknight10-cyber <jack.manjarres10@gmail.com>
2026-05-06 21:09:46 -0400

    paracticando variables y bugs

---
+++ python/# bugs.py
@@ -0,0 +1,12 @@
+# solo por que salia en el viseo pero es demasiado generico
+print("Hola mundo")
+butterflies = 10
+beetles = 12
+ladybugs = 20
+
+print('I caught ' + str(butterflies) + ' 🦋 butterflies!')
+print('I caught ' + str(beetles) + ' 🪲 beetles!')
+print('I caught ' + str(ladybugs) + " 🐞 ladybugs!")
+total = butterflies + beetles + ladybugs
+print('I caught ' + str(total) + ' total bugs!')
+# tambien puedo usar print(f-string)
